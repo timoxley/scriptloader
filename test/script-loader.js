331949eb6c4a54a762c69a7d1d33c995b3f7933e
Tim Oxley <secoif@gmail.com>
2012-11-20 08:31:04 +1000

    Testing...

--- test/script-loader.js
+++ test/script-loader.js
@@ -1,6 +1,7 @@
 var loadScript = require('script-loader')
 if (!window.testPassed) {
   it('can load remote scripts', function(done) {
+    debugger
     loadScript('https://raw.github.com/timoxley/script-loader/master/test/script-loader.js')
     window.testPassed = done
   })
