8fac6a9c84fbd0dd15079f991c1670322dcd4913
Tim Oxley <secoif@gmail.com>
2012-11-20 09:07:42 +1000

    Testing...

--- test/script-loader.js
+++ test/script-loader.js
@@ -1,13 +1,16 @@
 describe('script loader', function() {
+  beforeEach(function() {
+    mocha.options.globals.push['testPassed']
+  })
   var loadScript = require('script-loader')
   if (!window.testPassed) {
     it('can load remote scripts', function(done) {
-      mocha.options.globals.push['testPassed']
       window.testPassed = done
       loadScript('https://raw.github.com/timoxley/script-loader/master/test/script-loader.js')
     })
   } else {
-    mocha.options.globals.push['testPassed']
-    window.testPassed()
+    it('loaded remote script', function() {
+      window.testPassed()
+    })
   }
 })
