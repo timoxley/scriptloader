f881a6adb0d6370fcc6379259b1c7d9ea696e134
Tim Oxley <secoif@gmail.com>
2012-11-20 08:28:53 +1000

    Testing...

--- test/script-loader.js
+++ test/script-loader.js
@@ -1,18 +1,10 @@
 var loadScript = require('script-loader')
-console.log('yo')
-//window.done && window.done() && (function() {throw new Error()})
-window.count = window.count || 0
-window.count++
-console.log('1', window.count)
-if (window.count < 3) loadScript('https://raw.github.com/timoxley/script-loader/master/test/script-loader.js')
-console.log('2', window.count)
-//it('can load remote scripts', function(done) {
-  // set callback to a global var so it can be run by remote script
-  //debugger
-  //window.testPassed = done
-//})
-//setTimeout(function() {
-  //debugger
-//}, 2000)
-
+if (!window.testPassed) {
+  it('can load remote scripts', function(done) {
+    loadScript('https://raw.github.com/timoxley/script-loader/master/test/script-loader.js')
+    window.testPassed = done
+  })
+} else {
+  window.testPassed()
+}
 
