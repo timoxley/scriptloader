8a8e347186bbd327b216de13f05e50ad3e956349
Tim Oxley <secoif@gmail.com>
2012-11-20 08:59:42 +1000

    Testing...

--- test/script-loader.js
+++ test/script-loader.js
@@ -1,9 +1,9 @@
 var loadScript = require('script-loader')
 if (!window.testPassed) {
   it('can load remote scripts', function(done) {
-    loadScript('https://raw.github.com/timoxley/script-loader/master/test/script-loader.js')
     mocha.options.globals.push['testPassed']
     window.testPassed = done
+    loadScript('https://raw.github.com/timoxley/script-loader/master/test/script-loader.js')
   })
 } else {
   mocha.options.globals.push['testPassed']
